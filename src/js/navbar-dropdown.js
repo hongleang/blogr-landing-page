58c55e9f4a89b6fc469670e7d028d2c39cb158a5
hongleang <lim.hongleang@gmail.com>
2021-08-11 21:22:44 +1000

    navdropdown js

--- src/js/navbar-dropdown.js
+++ src/js/navbar-dropdown.js
@@ -1,6 +1,7 @@
 const navDropdownItems = document.querySelectorAll('.nav-dropdown-item');
 const navDropdownItemsChildren = document.querySelectorAll('.nav-dropdown-item__childrens');
 
+
 const navDropdown = (event) => {
   const item = event.currentTarget;
   const arrow = item.children[0].children[0];
